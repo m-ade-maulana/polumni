47bc0e344292055a2a03111134b73d0cf3a38ba0
ademaulana12 <ademaulana464@gmail.com>
2022-03-01 17:25:14 +0700

    Add Function input biodata and fixing bug

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -27,6 +27,17 @@ class Auth extends CI_Controller
         $this->load->view('register', $data);
     }
 
+    public function biodata()
+    {
+        $data['title'] = "Biodata";
+
+        $data['id_account'] = $this->session->userdata('id_account');
+        $data['nama'] = $this->session->userdata('nama');
+        $data['tanggal_lahir'] = $this->session->userdata('tanggal_lahir');
+
+        $this->load->view('biodata', $data);
+    }
+
     public function activation($token)
     {
         $data['title'] = "Portal Alumni - Aktivasi";
@@ -37,6 +48,7 @@ class Auth extends CI_Controller
 
     public function login()
     {
+
         $this->form_validation->set_rules('username', 'Username', 'trim|required');
         $this->form_validation->set_rules('password', 'Password', 'required|min_length[6]');
 
@@ -52,62 +64,101 @@ class Auth extends CI_Controller
             if ($result) {
                 if ($result['username'] == $username) {
                     if (password_verify($password, $result['password'])) {
-                        if ($result['is_active'] == 1) {
+                        $cek_data = $this->dbm->read_part('tb_data_diri', 'nama', $result['nama'])->num_rows();
+                        if ($cek_data) {
+                            if ($cek_data['is_active'] == 1) {
+                                $data = [
+                                    'id_account' => $result['id_account'],
+                                    'nama' => $result['nama'],
+                                    'tanggal_lahir' => $result['tanggal_lahir'],
+                                    'email' => $result['email']
+                                ];
+                                $this->session->set_userdata($data);
+                                redirect('user', $data);
+                            } else {
+                                $this->session->set_flashdata(
+                                    'message',
+                                    '<script>
+                                        Swal.fire({
+                                            Position: "top-end",
+                                            icon: "error",
+                                            title: "Sesi Validasi",
+                                            text: "Data Anda Sedang Di Validasi",
+                                            showConfirmButton: false,
+                                            timer: 4000
+                                        })
+                                    </script>'
+                                );
+                                redirect('auth');
+                            }
+                        } else {
+                            $this->session->set_flashdata(
+                                'message',
+                                '<script>
+                                    Swal.fire({
+                                        Position: "top-end",
+                                        icon: "question",
+                                        title: "Selamat Datang",
+                                        text: "Silahkan Lengkapi Biodata Anda",
+                                        showConfirmButton: false,
+                                        timer: 2000
+                                    })
+                                </script>'
+                            );
                             $data = [
                                 'id_account' => $result['id_account'],
                                 'nama' => $result['nama'],
                                 'tanggal_lahir' => $result['tanggal_lahir'],
-                                'email' => $result['email'],
-                                'is_active' => $result['is_active']
+                                'email' => $result['email']
                             ];
-
                             $this->session->set_userdata($data);
-                            redirect('user', $data);
-                        } else {
-                            $this->session->set_flashdata(
-                                'message',
-                                '<div class="alert alert-danger" role="alert">
-                            <strong>Failed !</strong> Akun anda belum aktif silahkan aktifkan dahulu
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>'
-                            );
-                            redirect('auth');
+                            redirect('auth/biodata', $data);
                         }
                     } else {
                         $this->session->set_flashdata(
                             'message',
-                            '<div class="alert alert-danger" role="alert">
-                            <strong>Failed !</strong> Password anda salah
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>'
+                            '<script>
+                                Swal.fire({
+                                    Position: "top-end",
+                                    icon: "error",
+                                    title: "Gagal",
+                                    text: "Password Anda Salah, Silahkan Di Ulang",
+                                    showConfirmButton: false,
+                                    timer: 2000
+                                })
+                            </script>'
                         );
                         redirect('auth');
                     }
                 } else {
                     $this->session->set_flashdata(
                         'message',
-                        '<div class="alert alert-danger" role="alert">
-                        <strong>Failed !</strong> Username anda salah
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>'
+                        '<script>
+                            Swal.fire({
+                                Position: "top-end",
+                                icon: "error",
+                                title: "Gagal",
+                                text: "Username Anda Salah, Silahkan Di Ulang",
+                                showConfirmButton: false,
+                                timer: 2000
+                            })
+                        </script>'
                     );
                     redirect('auth');
                 }
             } else {
                 $this->session->set_flashdata(
                     'message',
-                    '<div class="alert alert-danger" role="alert">
-                        Data tidak ditemukan
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>'
+                    '<script>
+                        Swal.fire({
+                            Position: "top-end",
+                            icon: "error",
+                            title: "Gagal",
+                            text: "Akun Anda Tidak Di Temukan",
+                            showConfirmButton: false,
+                            timer: 2000
+                        })
+                    </script>'
                 );
                 redirect('auth');
             }
@@ -120,7 +171,6 @@ class Auth extends CI_Controller
         // $this->validation->reg_valid();
 
         $this->form_validation->set_rules('nama', 'Nama', 'required');
-        $this->form_validation->set_rules('tanggalLahir', 'TanggalLahir', 'required');
         $this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[tb_registered.email]');
         $this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[6]|is_unique[tb_registered.username]');
         $this->form_validation->set_rules('password', 'Password', 'required|min_length[6]|max_length[12]');
@@ -131,12 +181,11 @@ class Auth extends CI_Controller
 
             $this->load->view('register', $data);
         } else {
-            $string = 'aktifkan';
-            $token = sha1($string);
+            $id_account = random_int(000000, 999999);
+            $token = sha1($id_account);
             $data = [
-                'id_account' => random_int(000000, 999999),
+                'id_account' => $id_account,
                 'nama' => htmlspecialchars($this->input->post('nama')),
-                'tanggal_lahir' => $this->input->post('tanggalLahir'),
                 'email' => htmlspecialchars($this->input->post('email')),
                 'username' => htmlspecialchars($this->input->post('username')),
                 'password' => password_hash(htmlspecialchars($this->input->post('password')), PASSWORD_DEFAULT),
@@ -146,30 +195,40 @@ class Auth extends CI_Controller
             ];
 
             $insert = $this->dbm->create('tb_registered', $data);
+            // var_dump($data);
             if ($insert) {
                 $sending = $this->email_message->message($this->input->post('email'), $token);
 
                 if ($sending == TRUE) {
                     $this->session->set_flashdata(
                         'message',
-                        '<div class="alert alert-success" role="alert">
-                        <strong>Sukses!</strong> silahkan periksa email anda untuk aktivasi
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>'
+                        '<script>
+                            Swal.fire({
+                                Position: "top-end",
+                                icon: "success",
+                                title: "Berhasil Disimpan",
+                                text: "Sialhkan Login",
+                                showConfirmButton: false,
+                                timer: 2000
+                            })
+                        </script>'
                     );
                     redirect('auth');
                     // var_dump($sending);
+
                 } else {
                     $this->session->set_flashdata(
                         'message',
-                        '<div class="alert alert-success" role="alert">
-                        <strong>Sukses!</strong> jika email aktivasi tidak terkirim silahkan hubungi admin
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>'
+                        '<script>
+                            Swal.fire({
+                                Position: "top-end",
+                                icon: "failed",
+                                title: "Gagal Disimpan",
+                                text: "Silahkan di ulang",
+                                showConfirmButton: false,
+                                timer: 2000
+                            })
+                        </script>'
                     );
                     redirect('auth');
                     // var_dump($sending);
@@ -177,14 +236,122 @@ class Auth extends CI_Controller
             } else {
                 $this->session->set_flashdata(
                     'message',
-                    '<div class="alert alert-danger" role="alert">
-                    <strong>Gagal!</strong> menambahkan data
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>'
+                    '<script>
+                        Swal.fire({
+                            Position: "top-end",
+                            icon: "failed",
+                            title: "Gagal Disimpan",
+                            text: "Silahkan di ulang",
+                            showConfirmButton: false,
+                            timer: 2000
+                        })
+                    </script>'
                 );
                 redirect('auth/registered');
+            }
+        }
+    }
+
+    public function input_biodata()
+    {
+        $id_account = htmlspecialchars($this->input->post('id_account'));
+
+        if ($this->form_validation->run() == FALSE) {
+            $data['title'] = "Portal Alumni - Registered";
+            $data['favicon'] = "logo.png";
+
+            $this->load->view('auth/biodata', $data);
+        } else {
+            // $id_account = $this->session->userdata('id_account');
+            $config['upload_path']          = './upload/image/profile';
+            $config['allowed_types']        = 'jpg|png';
+            $config['max_size']             = 500;
+            $config['file_name']            = $id_account;
+            // $config['encrypt_name'] 		= true;
+            $this->load->library('upload', $config);
+
+            if (!$this->upload->do_upload('foto_profile')) {
+                if ($this->upload->data('file_size') > 500) {
+                    $this->session->set_flashdata(
+                        'message',
+                        '<script>
+                            Swal.fire({
+                                Position: "top-end",
+                                icon: "failed",
+                                title: "Gagal",
+                                text: "Maksimal gambar 500kb",
+                                showConfirmButton: false,
+                                timer: 2000
+                            })
+                        </script>'
+                    );
+                    redirect('auth/biodata');
+                }
+                $this->session->set_flashdata(
+                    'message',
+                    '<script>
+                        Swal.fire({
+                            Position: "top-end",
+                            icon: "failed",
+                            title: "Gagal",
+                            text: "Maksimal gambar 500kb",
+                            showConfirmButton: false,
+                            timer: 2000
+                        })
+                    </script>'
+                );
+                redirect('auth/biodata');
+            } else {
+                $upload = $this->upload->data();
+                $data = [
+                    'id_account' => $id_account,
+                    'nisn' => htmlspecialchars($this->input->post('nisn')),
+                    'nama' => htmlspecialchars($this->input->post('nama')),
+                    'tempat_lahir' => htmlspecialchars($this->input->post('tempat_lahir')),
+                    'tanggal_lahir' => $this->input->post('tanggal_lahir'),
+                    'jenis_kelamin' => htmlspecialchars($this->input->post('jenis_kelamin')),
+                    'agama' => htmlspecialchars($this->input->post('agama')),
+                    'telepon' => htmlspecialchars($this->input->post('telepon')),
+                    'email' => htmlspecialchars($this->input->post('email')),
+                    'tahun_lulusan' => htmlspecialchars($this->input->post('tahun_lulus')),
+                    'alamat' => htmlspecialchars($this->input->post('alamat')),
+                    'foto' => $upload['file_name'],
+                ];
+                $result = $this->db->insert('tb_data_diri', $data);
+                // var_dump($data);
+                if ($result) {
+                    $this->session->set_flashdata(
+                        'message',
+                        '<script>
+                            Swal.fire({
+                                Position: "top-end",
+                                icon: "success",
+                                title: "Berhasil",
+                                text: "Biodata sudah di lengkapi, silahkan tunggu 1x24 jam untuk sesi validasi",
+                                showConfirmButton: false,
+                                timer: 2000
+                            })
+                        </script>'
+                    );
+                    redirect('auth');
+                    // var_dump($result);
+                } else {
+                    $this->session->set_flashdata(
+                        'message',
+                        '<script>
+                            Swal.fire({
+                                Position: "top-end",
+                                icon: "failed",
+                                title: "Gagal",
+                                text: "Gagal menyimpan data, silahkan di ulang",
+                                showConfirmButton: false,
+                                timer: 2000
+                            })
+                        </script>'
+                    );
+                    redirect('auth/biodata');
+                    // var_dump($result);
+                }
             }
         }
     }
