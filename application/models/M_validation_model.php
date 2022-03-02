10efa660cbc28a88e196d576dbfac68cafb90d78
ademaulana12 <ademaulana464@gmail.com>
2022-03-02 16:42:14 +0700

    Revision validation

--- application/models/M_validation_model.php
+++ application/models/M_validation_model.php
@@ -10,6 +10,68 @@ class M_validation_model extends CI_Model
         $this->load->library('form_validation');
     }
 
+    public function reg_valid()
+    {
+        $config = [
+            [
+                'field' => 'nama',
+                'label' => 'Nama',
+                'rules' => 'required',
+                'error' => [
+                    'required' => 'Nama tidak boleh kosong'
+                ]
+            ],
+            [
+                'field' => 'tanggal_lahir',
+                'label' => 'Tanggal Lahir',
+                'rules' => 'required',
+                'error' => [
+                    'required' => 'Tanggal lahir harus di isi'
+                ]
+            ],
+            [
+                'field' => 'telepon',
+                'label' => 'Telepon',
+                'rules' => 'required|numeric',
+                'error' => [
+                    'required' => 'Tanggal lahir harus di isi',
+                    'numeric' => 'Nomor telepon harus di isi'
+                ]
+            ],
+            [
+                'field' => 'email',
+                'label' => 'Email',
+                'rules' => 'required|trim|valid_email|is_unique[tb_registered.email]',
+                'error' => [
+                    'required' => 'Tanggal lahir harus di isi',
+                    'valid_email' => 'Email tidak valid',
+                    'is_unique' => 'Email sudah terdaftar'
+                ]
+            ],
+            [
+                'field' => 'username',
+                'label' => 'Username',
+                'rules' => 'required|trim|min_length[6]|is_unique[tb_registered.username]',
+                'error' => [
+                    'required' => 'Tanggal lahir harus di isi',
+                    'is_unique' => 'Username sudah ada'
+                ]
+            ],
+            [
+                'field' => 'password',
+                'label' => 'Password',
+                'rules' => 'required|trim|min_length[6]|max_length[12]',
+                'error' => [
+                    'required' => 'Tanggal lahir harus di isi',
+                    'min_length' => 'Minimal 6 karakter',
+                    'max_length' => 'Maksimal 12 karakter'
+                ]
+            ]
+        ];
+
+        $this->form_validation->set_rules($config);
+    }
+
     public function bio_valid()
     {
         $config = [
